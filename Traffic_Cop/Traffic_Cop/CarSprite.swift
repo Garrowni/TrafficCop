58a8004dd71bc3d185765a29639e68221bcb18ce
mathewmanton <mathewmanton@hotmail.com>
2015-06-08 16:33:47 -0400

    Car Spawning
    we can now spawn cars. As well project is set up to start making scenes

--- Traffic_Cop/Traffic_Cop/CarSprite.swift
+++ Traffic_Cop/Traffic_Cop/CarSprite.swift
@@ -11,62 +11,76 @@ import SpriteKit
 
 
 
-class CarSprite
+class CarSprite : SKSpriteNode
 {
     
-    var _car : SKSpriteNode
-    var _spawn  : CGPoint
-    var _accelX : Float
-    var _accelY : Float
-    var _type : Int
-    var _textures : [SKTexture] = []
+     var _car : SKTexture
+     var _spawn  : CGPoint
+     var _accelX : Float
+     var _accelY : Float
+     var _type : Int
+     var _textures : [SKTexture] = []
+     var _size : CGSize
     
     
     
-    init(type : Int , spawnX : CGFloat, spawnY : CGFloat)
+    init(type : Int , spawn : CGPoint)
     {
+        
         self._type = type
         
         switch(self._type)
         {
         case 1:
-            self._car = SKSpriteNode(imageNamed: "Ambulence1")
-            self._spawn = CGPoint(x: spawnX, y: spawnY)
+            
+            self._car = SKTexture(imageNamed: "Ambulence1")
+            self._spawn = spawn
             self._accelX = 0
             self._accelY = 0
+            self._size = CGSize(width: 152, height: 66)
             
         case 2:
-            self._car = SKSpriteNode(imageNamed:"car_cop")
-            self._spawn = CGPoint(x: spawnX, y: spawnY)
+            self._car = SKTexture(imageNamed:"car_cop")
+            self._spawn = spawn
             self._accelX = 0
             self._accelY = 0
+            self._size = CGSize(width: 152, height: 66)
         case 3:
-            self._car = SKSpriteNode(imageNamed: "car_blue")
-           self._spawn = CGPoint(x: spawnX, y: spawnY)
+            self._car = SKTexture(imageNamed: "car_blue")
+           self._spawn = spawn
             self._accelX = 0
             self._accelY = 0
+            self._size = CGSize(width: 152, height: 66)
         case 4:
-            self._car = SKSpriteNode(imageNamed: "car_red")
-            self._spawn = CGPoint(x: spawnX, y: spawnY)
+            self._car = SKTexture(imageNamed: "car_red")
+            self._spawn = spawn
             self._accelX = 0
             self._accelY = 0
+            self._size = CGSize(width: 152, height: 66)
         case 5:
-            self._car = SKSpriteNode(imageNamed: "pickup_green")
-            self._spawn = CGPoint(x: spawnX, y: spawnY)
+            self._car = SKTexture(imageNamed: "pickup_green")
+            self._spawn = spawn
             self._accelX = 0
             self._accelY = 0
+            self._size = CGSize(width: 152, height: 66)
         case 6:
-            self._car = SKSpriteNode(imageNamed: "truck")
-            self._spawn = CGPoint(x: spawnX, y: spawnY)
+            self._car = SKTexture(imageNamed: "truck")
+            self._spawn = spawn
             self._accelX = 0
             self._accelY = 0
+            self._size = CGSize(width: 152, height: 66)
         default :
-            self._car = SKSpriteNode(imageNamed: "")
-            self._spawn = CGPoint(x: spawnX, y: spawnY)
+            self._car = SKTexture(imageNamed: "")
+            self._spawn = spawn
             self._accelX = 0
             self._accelY = 0
+            self._size = CGSize(width: 0, height: 0)
             
         }
+        super.init(texture: _car, color: nil, size: self._size)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder: ) has not been implemented")
     }
     func update()
     {}
--- Traffic_Cop/Traffic_Cop/GameViewController.swift
+++ Traffic_Cop/Traffic_Cop/GameViewController.swift
@@ -9,29 +9,13 @@
 import UIKit
 import SpriteKit
 
-extension SKNode {
-    class func unarchiveFromFile(file : String) -> SKNode? {
-        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
-            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
-            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-            
-            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
-            archiver.finishDecoding()
-            return scene
-        } else {
-            return nil
-        }
-    }
-}
 
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
+        let scene =
+        GameScene(size: CGSize(width: 1080, height: 1920))
             let skView = self.view as! SKView
             skView.showsFPS = true
             skView.showsNodeCount = true
@@ -44,24 +28,9 @@ class GameViewController: UIViewController {
             
             skView.presentScene(scene)
         }
-    }
+    
 
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
 
-    override func supportedInterfaceOrientations() -> Int {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
-        } else {
-            return Int(UIInterfaceOrientationMask.All.rawValue)
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
 
     override func prefersStatusBarHidden() -> Bool {
         return true
