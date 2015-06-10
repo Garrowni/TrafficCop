f7d0ada19ad22518dfef70e64ce10991b4254f6f
JustinMorritt <justinmorritt@Justins-Mac-mini.local>
2015-06-10 15:14:16 -0400

    stuff

--- Traffic_Cop/DrawText.swift
+++ Traffic_Cop/DrawText.swift
@@ -6,20 +6,43 @@
 //  Copyright (c) 2015 Mat_Nicole_Justin. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import SpriteKit
 
-class Text
+class Text : SKLabelNode
 {
+    var says: String = ""
+    var fontSize : Int = 0
+    var align : String = ""
     
-    init(says: String, fontSize: Int , font: String)
+    
+    init(says: String, fontSize: Int , font: String, align: String)
     {
-        switch(font)
-        {
-            case "font1":println("")
-            default:println("")
-        }
+        
 
+        let label = SKLabelNode(fontNamed: Marion)
+        
+//        switch(font)
+//        {
+//            case "font1":label = SKLabelNode(fontNamed:Marion)
+//                label.text = says
+//                label.position = CGPoint(x: size.width / 2, y: (size.height * (CGFloat(idx+1))) / (CGFloat(fontNames.count)+1))
+//                label.fontSize = fontSize
+//                switch(align)
+//                {
+//                    case "center":label.verticalAlignmentMode = .Center
+//                }
+//            
+//            
+//                addChild(label)
+//            
+//            
+//            
+//            
+//            
+//            default:println("")
+//        }
+//
        
     }
     
