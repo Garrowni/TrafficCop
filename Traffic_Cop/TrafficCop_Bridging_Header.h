e92a866d465b60d3bac49d060e12e849c49cc210
mathewmanton <mathewmanton@hotmail.com>
2015-06-10 13:31:24 -0400

    TMX Class started

---
+++ Traffic_Cop/TrafficCop_Bridging_Header.h
@@ -0,0 +1,14 @@
+//
+//  TrafficCop_Bridging_Header.h
+//  Traffic_Cop
+//
+//  Created by Mathew  Manton on 2015-06-10.
+//  Copyright (c) 2015 Mat_Nicole_Justin. All rights reserved.
+//
+#import "JSTileMap.h"
+
+#ifndef Traffic_Cop_TrafficCop_Bridging_Header_h
+#define Traffic_Cop_TrafficCop_Bridging_Header_h
+
+
+#endif
