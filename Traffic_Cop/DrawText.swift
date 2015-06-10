484f89cd949022b21ed81d09039d5ee9756b3467
JustinMorritt <justinmorritt@Justins-Mac-mini.local>
2015-06-10 14:47:55 -0400

    added text class

---
+++ Traffic_Cop/DrawText.swift
@@ -0,0 +1,34 @@
+//
+//  DrawText.swift
+//  Traffic_Cop
+//
+//  Created by Justin Morritt on 2015-06-10.
+//  Copyright (c) 2015 Mat_Nicole_Justin. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Text
+{
+    
+    init(says: String, fontSize: Int , font: String)
+    {
+        switch(font)
+        {
+            case "font1":println("")
+            default:println("")
+        }
+
+       
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder: ) has not been implemented")
+    }
+
+
+
+}
+
