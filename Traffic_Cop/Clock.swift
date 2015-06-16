7b8e5cac422e78b7a56111177d6b2c01968a6111
JustinMorritt <justinmorritt@Justins-Mac-mini.local>
2015-06-16 13:22:27 -0400

    sup

--- Traffic_Cop/Clock.swift
+++ Traffic_Cop/Clock.swift
@@ -6,35 +6,34 @@
 //  Copyright (c) 2015 Mat_Nicole_Justin. All rights reserved.
 //
 //
-//import Foundation
-//import SpriteKit
-//
-//
-//class Clock
-//{
-//    var time            : String
-//    let clockLabel      : Text
-//    let button1         : Button
-//    let clockR        : CGRect
-//    
-//    
-//    init()
-//    {
-//        time = "10:00"
-//        
-//        
-//        clockLabel  = Text(pos: CGPoint(x: 0, y: 0),    says: "time",        fontSize: 250, font: "font4", color: "green", align: "center")
-//        titleButtR  = CGRect(x: size.width/2-350, y: size.height-300, width: 700, height: 200)
-//        
-//        button1     = Button(pos: button1R,     roundCorner: 100, text: label1,     BGcolor: "blue", OLcolor: "red", OLSize: 10, glowWidth: 20, ZoomIn: true, Bulge: false, glowBulge: true)
-//        titleButt   = Button(pos: titleButtR,   roundCorner: 100, text: titleLabel, BGcolor: "blue", OLcolor: "red", OLSize: 10, glowWidth: 20, ZoomIn: true, Bulge: true, glowBulge: false)
-//    }
-//    
-//    
-//    func changeText(time: String)
-//    {
-//        
-//    }
-//    
-//    
-//}
+import Foundation
+import SpriteKit
+
+
+class Clock
+{
+    var time            : String
+    let clockLabel      : Text
+    let clockButt       : Button
+    let clockR          : CGRect
+    
+    
+    init()
+    {
+        time = "10:00"
+        
+        
+        clockLabel  = Text(pos: CGPoint(x: 0, y: 0),    says: time,  fontSize: 250, font: "font4", color: "green", align: "center")
+        clockR  = CGRect(x: size.width/2-350, y: size.height-300, width: 700, height: 200)
+        clockButt     = Button(pos: button1R,     roundCorner: 100, text: label1,     BGcolor: "blue", OLcolor: "red", OLSize: 10, glowWidth: 20, ZoomIn: true, Bulge: false, glowBulge: true)
+        
+    }
+    
+    
+    func changeText(time: String)
+    {
+        
+    }
+    
+    
+}
