5d0e771aa521cd3a91c936c419f64289c386aa95
JustinMorritt <justinmorritt@Justins-Mac-mini.local>
2015-06-19 15:54:54 -0400

    points done

--- Traffic_Cop/pointsPopUp.swift
+++ Traffic_Cop/pointsPopUp.swift
@@ -25,10 +25,10 @@ class PointsPopUp
     {
         points = Points
         done = false
-        text = Text(pos: Pos, says: String(points), fontSize: 50, font: "font2", color: "green",  align: "center")
-        zoom = SKAction.scaleTo(CGFloat(1.0), duration: 1)
+        text = Text(pos: Pos, says: String(points), fontSize: 70, font: "font2", color: "green",  align: "center")
+        zoom = SKAction.scaleTo(CGFloat(1.0), duration: 0.5)
         move = SKAction.moveTo(goto, duration: 2)
-        zoomO = SKAction.scaleTo(CGFloat(0.0), duration: 1)
+        zoomO = SKAction.scaleTo(CGFloat(0.0), duration: 0.5)
 
         start()
     }
