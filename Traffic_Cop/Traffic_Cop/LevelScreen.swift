f215211b603177355809b43e1f94b41eba9a7652
JustinMorritt <justinmorritt@Justins-Mac-mini.local>
2015-06-25 16:55:05 -0400

    put wait back in

--- Traffic_Cop/Traffic_Cop/LevelScreen.swift
+++ Traffic_Cop/Traffic_Cop/LevelScreen.swift
@@ -218,14 +218,14 @@ class LevelScreen: SKScene
             let myScene2 = GameScene.level(lvl)
     
             myScene.scaleMode = self.scaleMode
-            let reveal = SKTransition.doorsCloseHorizontalWithDuration(1)
+            let reveal = SKTransition.doorsCloseHorizontalWithDuration(1.5)
             
             if lvl == 0
             {self.view?.presentScene(myScene, transition: reveal)}
             else{self.view?.presentScene(myScene2, transition: reveal)}
             
         }
-        let transSequence = SKAction.sequence([transition,block])
+        let transSequence = SKAction.sequence([transition,wait,block])
         self.runAction(transSequence)
     }
     
