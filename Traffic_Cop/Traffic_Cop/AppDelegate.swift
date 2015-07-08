b4979758497405418ed249d846e85e93981b8a54
JustinMorritt <justinmorritt@Justins-Mac-mini.local>
2015-07-08 13:23:27 -0400

    working again

--- Traffic_Cop/Traffic_Cop/AppDelegate.swift
+++ Traffic_Cop/Traffic_Cop/AppDelegate.swift
@@ -26,11 +26,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         //FETCH
+        let entity = NSEntityDescription.entityForName("SaveData", inManagedObjectContext: managedObjectContext!)
+        let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedObjectContext!)
+        
+        
         var fetchRequest = NSFetchRequest(entityName: "SaveData")
         let fetchResults = managedObjectContext!.executeFetchRequest(fetchRequest, error: &error) as? [CoreData]
         if let variables = fetchResults{
         var VAR = variables[0]
-        println("\(VAR.initialized.boolValue)")
+        //println("\(VAR.initialized.boolValue)")
         if(!VAR.initialized.boolValue)
             {
                 VAR.lev2 = NSNumber(bool: false)
@@ -44,7 +48,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 VAR.hsLev4 = NSNumber(int: 0)
                 VAR.initialized = NSNumber(bool: true)
                 
-                println("\(VAR.initialized.boolValue)")
+                //println("\(VAR.initialized.boolValue)")
                 
                 //SAVE
                 if managedObjectContext!.save(&error){ println("TRUE !! WAS INITIALIZED")
@@ -93,37 +97,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }()
     
     lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
         let modelURL = NSBundle.mainBundle().URLForResource("CoreData", withExtension: ".momd")!
         return NSManagedObjectModel(contentsOfURL: modelURL)!
         }()
     
     lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
-        // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        // Create the coordinator and store
         var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
         let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("Traffic_Cop.sqlite")
         var error: NSError? = nil
-        var failureReason = "There was an error creating or loading the application's saved data."
         if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {
-            coordinator = nil
-            // Report any error we got.
-            var dict = [String: AnyObject]()
-            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
-            dict[NSLocalizedFailureReasonErrorKey] = failureReason
-            dict[NSUnderlyingErrorKey] = error
-            error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            // Replace this with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog("Unresolved error \(error), \(error!.userInfo)")
-            abort()
+            // What to do when it fails
         }
         
         return coordinator
         }()
     
     lazy var managedObjectContext: NSManagedObjectContext? = {
-        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
         let coordinator = self.persistentStoreCoordinator
         if coordinator == nil {
             return nil
